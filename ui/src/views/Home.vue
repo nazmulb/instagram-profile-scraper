452cd8fcf685822633a098233a566a07ca2645a0
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-25 19:44:27 +0300

    added codes

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="row justify-content-center modash-home">
     <div class="col-12 col-sm-8 col-lg-7">
-      <div class="box modash-list">Dwayne Johnson</div>
-      <div class="box modash-list">Dwayne Johnson</div>
+      <div class="box-card modash-list">Dwayne Johnson</div>
+      <div class="box-card modash-list">Dwayne Johnson</div>
     </div>
   </div>
 </template>
@@ -23,6 +23,15 @@ export default {
 .modash-home {
   .modash-list {
     margin-bottom: 1rem;
+    min-height: 5rem;
+    transition: all 0.2s linear;
+    cursor: pointer;
+  }
+
+  .modash-list:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2),
+      0 1px 15px 0 rgba(0, 0, 0, 0.19);
   }
 }
 </style>
--- ui/src/views/Profile.vue
+++ ui/src/views/Profile.vue
@@ -3,17 +3,18 @@
     <div class="col-12 col-sm-8 col-lg-7">
       <div class="nav-back">
         <router-link to="/">
-          <img
-            src="../assets/images/back.svg"
-            width="23"
-            height="20"
-            alt="back"
-          />
+          <img src="../assets/images/back.svg" width="23" height="20" alt="back" />
         </router-link>
       </div>
-      <div class="box modash-profile-container">
-        <h2>Dwayne Johnson</h2>
-        <p>@therock</p>
+      <div class="box-card modash-profile-container">
+        <h5 class="profile-name">Dwayne Johnson</h5>
+        <p>
+          <a
+            class="profile-handle-link"
+            href="https://www.instagram.com/theaveryschrader"
+            target="_blank"
+          >@therock</a>
+        </p>
       </div>
     </div>
   </div>
@@ -24,12 +25,26 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/styles/variables";
 .modash-profile {
   .nav-back {
     margin: 0rem 0rem 1rem 1.2rem;
   }
   .modash-profile-container {
     text-align: center;
+
+    .profile-name {
+      font-weight: 400;
+    }
+
+    .profile-handle-link {
+      color: $profile-handle-link-color;
+
+      &:hover,
+      &:active {
+        color: $profile-handle-link-color;
+      }
+    }
   }
 }
 </style>
