be37dc032e66bb6015da04a96dcb3b8d8344ba15
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-26 01:15:12 +0300

    added codes

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="row justify-content-center modash-home">
     <div class="col-12 col-sm-8 col-lg-7">
-      <div v-if="getLoading" class="box-card text-center">Loading...</div>
+      <div v-if="getProfiles.length == 0 && getLoading" class="box-card text-center">
+        <div class="spinner-border text-info" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
       <div
         v-if="getProfiles.length == 0 && !getLoading"
         class="box-card text-center"
-      >
-        There is no profile to show!
+      >There is no profile to show!</div>
+      <div v-else>
+        <ProfileList v-for="profile in getProfiles" :key="profile.id" :profile="profile" />
       </div>
-      <ProfileList
-        v-for="profile in getProfiles"
-        :key="profile.id"
-        :profile="profile"
-      />
+      
     </div>
   </div>
 </template>
