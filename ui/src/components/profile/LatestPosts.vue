75ae5daf72da69b0325536d4d2a1282a381d3115
nazmulbasher <nazmul.basher@fieldnation.com>
2020-04-26 10:25:37 +0300

    added codes

--- ui/src/components/profile/LatestPosts.vue
+++ ui/src/components/profile/LatestPosts.vue
@@ -7,65 +7,17 @@
       alt="instagram"
     />
     <h6 class="section-header">Latest posts</h6>
-    <div class="box-card" v-for="post in posts" :key="post.id">
-      <p>{{ post.takenAtTimestamp }}</p>
-      <p v-if="!post.isVideo">
-        <img
-          :src="post.thumbnailUrl"
-          width="215"
-          height="190"
-          alt="Post Picture"
-        />
-      </p>
-      <p v-if="post.isVideo">
-        <video
-          class="posts-video"
-          width="215"
-          height="190"
-          controls
-          controlslist="nodownload"
-          playsinline
-          :poster="post.thumbnailUrl"
-          preload="metadata"
-          type="video/mp4"
-          :src="post.videoUrl"
-        ></video>
-      </p>
-      <p>
-        <span v-if="post.isVideo">
-          <img
-            src="../../assets/images/play.svg"
-            width="11"
-            height="11"
-            alt="play"
-          />
-          {{ post.videoViewCount }}
-        </span>
-        <span>
-          <img
-            src="../../assets/images/likes.svg"
-            width="15"
-            height="10"
-            alt="likes"
-          />
-          {{ post.totalLikes }}
-        </span>
-        <span>
-          <img
-            src="../../assets/images/comments.svg"
-            width="15"
-            height="12"
-            alt="comments"
-          />
-          {{ post.totalComments }}
-        </span>
-      </p>
-    </div>
+    <PostIteams v-for="post in posts" :key="post.id" :post="post" />
   </div>
 </template>
 
 <script>
+import PostIteams from "@/components/profile/PostIteams.vue";
+
 export default {
+  components: {
+    PostIteams
+  },
   props: {
     posts: {
       type: Array,
---
+++ ui/src/components/profile/PostIteams.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="box-card">
+    <p>{{ post.takenAtTimestamp }}</p>
+    <p v-if="!post.isVideo">
+      <img
+        :src="post.thumbnailUrl"
+        width="215"
+        height="190"
+        alt="Post Picture"
+      />
+    </p>
+    <p v-if="post.isVideo">
+      <video
+        class="posts-video"
+        width="215"
+        height="190"
+        controls
+        controlslist="nodownload"
+        playsinline
+        :poster="post.thumbnailUrl"
+        preload="metadata"
+        type="video/mp4"
+        :src="post.videoUrl"
+      ></video>
+    </p>
+    <p>
+      <span v-if="post.isVideo">
+        <img
+          src="../../assets/images/play.svg"
+          width="11"
+          height="11"
+          alt="play"
+        />
+        {{ post.videoViewCount }}
+      </span>
+      <span>
+        <img
+          src="../../assets/images/likes.svg"
+          width="15"
+          height="10"
+          alt="likes"
+        />
+        {{ post.totalLikes }}
+      </span>
+      <span>
+        <img
+          src="../../assets/images/comments.svg"
+          width="15"
+          height="12"
+          alt="comments"
+        />
+        {{ post.totalComments }}
+      </span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
